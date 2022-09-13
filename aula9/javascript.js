af2526b90faaf344c3876a089ddcff8a3a22d2bb
gu-nog <gumendes.nog.dev@gmail.com>
2022-09-13 12:25:55 -0300

    files used in my tests and notes from 9th class

---
+++ aula9/javascript.js
@@ -0,0 +1,14 @@
+let count = 0;
+count = 0;
+count++;
+if (count == 1)
+{
+    while (true)
+    {
+        for (let c = 0; c == 0; c++)
+        {
+            console.log("I'm working and I can modify the tree of html elements and also some css/DOM");
+        }
+        break;
+    }
+}
